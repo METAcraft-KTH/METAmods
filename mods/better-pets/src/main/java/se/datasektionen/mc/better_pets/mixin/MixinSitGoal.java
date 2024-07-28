0343004bc54e2e09d6d50247a1ef29b8c16afceb
Acuadragon100 <30689683+Acuadragon100@users.noreply.github.com>
2024-07-28 22:27:13 +0200

    Fix cats sitting down and refusing to stand when owner is too far away despite trusted player nearby.

---
+++ mods/better-pets/src/main/java/se/datasektionen/mc/better_pets/mixin/MixinSitGoal.java
@@ -0,0 +1,26 @@
+package se.datasektionen.mc.better_pets.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.goal.SitGoal;
+import net.minecraft.entity.passive.TameableEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import se.datasektionen.mc.better_pets.TameableExtension;
+
+@Mixin(SitGoal.class)
+public class MixinSitGoal {
+
+	@WrapOperation(
+			method = "canStart",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/entity/passive/TameableEntity;getOwner()Lnet/minecraft/entity/LivingEntity;"
+			)
+	)
+	public LivingEntity checkFollowTarget(TameableEntity instance, Operation<LivingEntity> original) {
+		return ((TameableExtension) instance).metacraft$getCurrentFollowTarget();
+	}
+
+}
