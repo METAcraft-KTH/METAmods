70b83335bad742b4e7bd4a9dde8914b26001d1e9
Acuadragon100 <30689683+Acuadragon100@users.noreply.github.com>
2025-03-26 23:29:36 +0100

    Make it possible to enchant totems of undying.

--- mods/metacraft-season-4/src/main/java/se/datasektionen/mc/metacraft_season_4/Events.java
+++ mods/metacraft-season-4/src/main/java/se/datasektionen/mc/metacraft_season_4/Events.java
@@ -1,6 +1,9 @@
 package se.datasektionen.mc.metacraft_season_4;
 
+import eu.pb4.polymer.core.api.item.PolymerItemUtils;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.item.Items;
 import se.datasektionen.mc.metacraft_season_4.end.EndBossPlayerState;
 
 public class Events {
@@ -9,6 +12,9 @@ public class Events {
 		ServerTickEvents.END_WORLD_TICK.register(world -> {
 			EndBossPlayerState.getInstance(world).ifPresent(inst -> inst.tick(world));
 		});
+
+		PolymerItemUtils.ITEM_CHECK.register(stack -> stack.isOf(Items.TOTEM_OF_UNDYING));
+		PolymerItemUtils.syncDefaultComponent(Items.TOTEM_OF_UNDYING, DataComponentTypes.ENCHANTABLE);
 	}
 
 }
