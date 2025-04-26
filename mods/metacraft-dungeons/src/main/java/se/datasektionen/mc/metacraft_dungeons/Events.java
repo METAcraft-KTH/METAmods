b487bfef2ae77f954550772724fb18a6d0d68162
Acuadragon100 <30689683+Acuadragon100@users.noreply.github.com>
2025-04-26 16:26:18 +0200

    Add dungeons data to dungeon dimension if not present.

--- mods/metacraft-dungeons/src/main/java/se/datasektionen/mc/metacraft_dungeons/Events.java
+++ mods/metacraft-dungeons/src/main/java/se/datasektionen/mc/metacraft_dungeons/Events.java
@@ -2,6 +2,7 @@ package se.datasektionen.mc.metacraft_dungeons;
 
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerBlockEntityEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
 import se.datasektionen.mc.metacraft_core.block.entities.MusicBlockEntity;
 import se.datasektionen.mc.metacraft_dungeons.dungeons.DungeonData;
 import se.datasektionen.mc.metacraft_dungeons.util.ChunkHelper;
@@ -12,6 +13,13 @@ public class Events {
 		ServerTickEvents.END_WORLD_TICK.register(world -> {
 			DungeonData.getIfPresent(world).ifPresent(DungeonData::tick);
 		});
+		ServerWorldEvents.LOAD.register(
+				(server, world) -> {
+					if (world.getRegistryKey() == Dimensions.DUNGEONS) {
+						DungeonData.getInstance(world); //Init data for dungeons dimension.
+					}
+				}
+		);
 		ServerBlockEntityEvents.BLOCK_ENTITY_LOAD.register((blockEntity, world) -> {
 			if (blockEntity instanceof MusicBlockEntity musicBlock) {
 				DungeonData.getIfPresent(world).ifPresent(data -> data.loadMusicBlock(musicBlock));
