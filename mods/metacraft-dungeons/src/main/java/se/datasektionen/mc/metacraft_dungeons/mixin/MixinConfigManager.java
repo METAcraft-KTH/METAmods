61c826015fde92c26cc0b573b9df7c9324743d44
Acuadragon100 <30689683+Acuadragon100@users.noreply.github.com>
2025-04-26 17:14:27 +0200

    Fix crash when resetting dungeon if squaremap banners is installed. Basically completely disables squaremap for the dungeon dimension.

---
+++ mods/metacraft-dungeons/src/main/java/se/datasektionen/mc/metacraft_dungeons/mixin/MixinConfigManager.java
@@ -0,0 +1,25 @@
+package se.datasektionen.mc.metacraft_dungeons.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.server.world.ServerWorld;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import se.datasektionen.mc.metacraft_dungeons.dungeons.DungeonData;
+import xyz.jpenilla.squaremap.common.config.ConfigManager;
+import xyz.jpenilla.squaremap.common.config.WorldConfig;
+
+@Pseudo
+@Mixin(ConfigManager.class)
+public class MixinConfigManager {
+
+	@ModifyReturnValue(method = "worldConfig", at = @At("RETURN"))
+	public WorldConfig worldConfig(WorldConfig config, @Local(argsOnly = true) ServerWorld world) {
+		if (DungeonData.getIfPresent(world).isPresent()) {
+			config.MAP_ENABLED = false;
+		}
+		return config;
+	}
+
+}
