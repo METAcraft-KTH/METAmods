1ea733e26e9240f55cd7a3f6d1ec1552c74efafc
Alvinn8 <42838560+Alvinn8@users.noreply.github.com>
2025-05-06 21:30:05 +0200

    Update cclan branch to 1.21.5

--- mods/metacraft-minigame-util/src/main/java/nu/metacraft/minigame_util/MinigameUtilState.java
+++ mods/metacraft-minigame-util/src/main/java/nu/metacraft/minigame_util/MinigameUtilState.java
@@ -1,56 +1,40 @@
 package nu.metacraft.minigame_util;
 
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.fabricmc.fabric.api.util.TriState;
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.nbt.NbtOps;
-import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.PersistentState;
+import net.minecraft.world.PersistentStateType;
+
+import java.util.Optional;
 
 public class MinigameUtilState extends PersistentState {
 
 	private static final String KEY = "metacraft-minigame-util";
 
-	private static final String BLOCK_SETTING_RULES = "BlockSettingRules";
-
 	private BlockPredicateList canBreak;
 
-	private static final PersistentState.Type<MinigameUtilState> TYPE = new Type<>(MinigameUtilState::new, MinigameUtilState::fromNBT, null);
+	private static final Codec<MinigameUtilState> CODEC = RecordCodecBuilder.create(
+		instance -> instance.group(
+			BlockPredicateList.CODEC.optionalFieldOf("canBreak").forGetter(state -> Optional.ofNullable(state.canBreak))
+		).apply(instance, MinigameUtilState::new)
+	);
 
-	private static MinigameUtilState fromNBT(NbtCompound nbt, RegistryWrapper.WrapperLookup registryLookup) {
-		var data = new MinigameUtilState();
-		data.readNBT(nbt, registryLookup);
-		return data;
+	private static final PersistentStateType<MinigameUtilState> TYPE = new PersistentStateType<>(
+		KEY, MinigameUtilState::new, CODEC, null
+	);
+
+	private MinigameUtilState() {}
+
+	public MinigameUtilState(Optional<BlockPredicateList> list) {
+		this.canBreak = list.orElse(null);
 	}
 
 	public static MinigameUtilState getInstance(MinecraftServer server) {
-		return server.getOverworld().getPersistentStateManager().getOrCreate(TYPE, KEY);
-	}
-
-	@Override
-	public NbtCompound writeNbt(NbtCompound nbt, RegistryWrapper.WrapperLookup registries) {
-		if (canBreak != null) {
-			BlockPredicateList.CODEC.encodeStart(
-					registries.getOps(NbtOps.INSTANCE),
-					canBreak
-			).resultOrPartial(MinigameUtil.LOGGER::error).ifPresent(result -> {
-				nbt.put(BLOCK_SETTING_RULES, result);
-			});
-		}
-		return nbt;
-	}
-
-	public void readNBT(NbtCompound nbt, RegistryWrapper.WrapperLookup registries) {
-		if (nbt.contains(BLOCK_SETTING_RULES)) {
-			BlockPredicateList.CODEC.parse(
-					registries.getOps(NbtOps.INSTANCE),
-					nbt.getCompound(BLOCK_SETTING_RULES)
-			).resultOrPartial(MinigameUtil.LOGGER::error).ifPresent(result -> {
-				canBreak = result;
-			});
-		}
+		return server.getOverworld().getPersistentStateManager().getOrCreate(TYPE);
 	}
 
 	public void setCanBreak(BlockPredicateList canBreak) {
