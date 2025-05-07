50056b81a434921a4b2589d0aedb3c163ec4435c
Acuadragon100 <30689683+Acuadragon100@users.noreply.github.com>
2025-05-07 17:19:58 +0200

    Fix portals being destroyed by fluids.

---
+++ mods/metacraft-core/src/main/java/se/datasektionen/mc/metacraft_core/mixin/MixinFlowableFluid.java
@@ -0,0 +1,25 @@
+package se.datasektionen.mc.metacraft_core.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import net.minecraft.block.BlockState;
+import net.minecraft.fluid.FlowableFluid;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import se.datasektionen.mc.metacraft_core.block.blocks.PortalCore;
+import se.datasektionen.mc.metacraft_core.block.blocks.PortalPadding;
+
+@Mixin(FlowableFluid.class)
+public class MixinFlowableFluid {
+
+	@ModifyReturnValue(
+		method = "canFill(Lnet/minecraft/block/BlockState;)Z",
+		at = @At("RETURN")
+	)
+	private static boolean canFill(boolean original, BlockState state) {
+		if (state.getBlock() instanceof PortalCore || state.getBlock() instanceof PortalPadding) {
+			return false;
+		}
+		return original;
+	}
+
+}
