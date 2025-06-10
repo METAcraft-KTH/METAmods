10cae0b13b2d2eab04fb80ec5d83a2870888edb8
Acuadragon100 <30689683+Acuadragon100@users.noreply.github.com>
2025-06-10 17:17:30 +0200

    Made moving markers also able to rotate and never let path time be 0.

--- mods/metacraft-core/src/main/java/se/datasektionen/mc/metacraft_core/entity/entities/MovingMarker.java
+++ mods/metacraft-core/src/main/java/se/datasektionen/mc/metacraft_core/entity/entities/MovingMarker.java
@@ -1,6 +1,7 @@
 package se.datasektionen.mc.metacraft_core.entity.entities;
 
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import eu.pb4.polymer.core.api.entity.PolymerEntity;
 import it.unimi.dsi.fastutil.doubles.DoubleList;
 import net.minecraft.entity.EntityType;
@@ -23,7 +24,7 @@ public class MovingMarker extends MarkerEntity implements PolymerEntity {
 	public static final String PATH_PROGRESS = "path_progress";
 
 	private static final Codec<InterpolationSet<MovingMarker, Target>> PATH_CODEC = InterpolationSet.createCodec(
-			Target.CODEC.fieldOf("pos"), Target::fromStream
+			Target.CODEC.fieldOf("target"), Target::fromStream
 	);
 
 	private int pathTime = 100;
@@ -44,8 +45,11 @@ public class MovingMarker extends MarkerEntity implements PolymerEntity {
 	public void tick() {
 		super.tick();
 		if (path != null) {
-			var pos = path.interpolate((double) pathProgress / pathTime).pos;
-			refreshPositionAndAngles(pos.getX(), pos.getY(), pos.getZ(), this.getYaw(), this.getPitch());
+			if (pathTime == 0) {
+				pathTime = 100;
+			}
+			var pos = path.interpolate((double) pathProgress / pathTime);
+			refreshPositionAndAngles(pos.pos.getX(), pos.pos.getY(), pos.pos.getZ(), pos.yaw, pos.pitch);
 			pathProgress++;
 			if (pathProgress > pathTime) {
 				pathProgress = 0;
@@ -63,8 +67,8 @@ public class MovingMarker extends MarkerEntity implements PolymerEntity {
 			).ifPresent(p -> path = p);
 
 			if (path != null) {
-				path = path.setStartIfNotPresent(new Target(this.getPos()));
-				path = path.setEndIfNotPresent(new Target(this.getPos()));
+				path = path.setStartIfNotPresent(new Target(this.getPos(), getYaw(), getPitch()));
+				path = path.setEndIfNotPresent(new Target(this.getPos(), getYaw(), getPitch()));
 			}
 		} else {
 			path = null;
@@ -84,18 +88,24 @@ public class MovingMarker extends MarkerEntity implements PolymerEntity {
 		nbt.putInt(PATH_PROGRESS, pathProgress);
 	}
 
-	public record Target(Vec3d pos) implements Interpolatable<MovingMarker> {
+	public record Target(Vec3d pos, float yaw, float pitch) implements Interpolatable<MovingMarker> {
 
-		public static final Codec<Target> CODEC = Vec3d.CODEC.xmap(Target::new, Target::pos);
+		public static final Codec<Target> CODEC = RecordCodecBuilder.create(
+				instance -> instance.group(
+						Vec3d.CODEC.fieldOf("pos").forGetter(Target::pos),
+						Codec.FLOAT.optionalFieldOf("yaw", 0f).forGetter(Target::yaw),
+						Codec.FLOAT.optionalFieldOf("pitch", 0f).forGetter(Target::pitch)
+				).apply(instance, Target::new)
+		);
 
 		@Override
 		public DoubleList getValues(MovingMarker context) {
-			return DoubleList.of(pos.getX(), pos.getY(), pos.getZ());
+			return DoubleList.of(pos.getX(), pos.getY(), pos.getZ(), yaw, pitch);
 		}
 
 		public static Target fromStream(DoubleStream stream) {
-			var arr = stream.limit(3).toArray();
-			return new Target(new Vec3d(arr[0], arr[1], arr[2]));
+			var arr = stream.limit(5).toArray();
+			return new Target(new Vec3d(arr[0], arr[1], arr[2]), (float) arr[3], (float) arr[4]);
 		}
 	}
 }
