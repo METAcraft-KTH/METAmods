87cca65bf7873ca07ce7a374378b78ddfb1b4f01
Acuadragon100 <30689683+Acuadragon100@users.noreply.github.com>
2025-06-17 16:35:07 +0200

    Fix playsound and particle commands not working properly when executed within the cutscene.

--- mods/metacraft-cutscenes/src/main/java/se/datasektionen/mc/cutscenes/mixin/MixinServerWorld.java
+++ mods/metacraft-cutscenes/src/main/java/se/datasektionen/mc/cutscenes/mixin/MixinServerWorld.java
@@ -1,21 +1,28 @@
 package se.datasektionen.mc.cutscenes.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import net.minecraft.entity.Entity;
 import net.minecraft.network.packet.Packet;
+import net.minecraft.registry.DynamicRegistryManager;
 import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.server.PlayerManager;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.MutableWorldProperties;
 import net.minecraft.world.World;
+import net.minecraft.world.dimension.DimensionType;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import se.datasektionen.mc.cutscenes.cutscene.world.CutsceneWorld;
 
 @Mixin(ServerWorld.class)
-public class MixinServerWorld {
+public abstract class MixinServerWorld extends World {
+
+	protected MixinServerWorld(MutableWorldProperties properties, RegistryKey<World> registryRef, DynamicRegistryManager registryManager, RegistryEntry<DimensionType> dimensionEntry, boolean isClient, boolean debugWorld, long seed, int maxChainedNeighborUpdates) {
+		super(properties, registryRef, registryManager, dimensionEntry, isClient, debugWorld, seed, maxChainedNeighborUpdates);
+	}
 
 	@WrapOperation(
 		method = "tickWeather",
@@ -59,6 +66,20 @@ public class MixinServerWorld {
 			return true;
 		}
 		return original;
+	}
+
+	@WrapOperation(
+		method = "sendToPlayerIfNearby",
+		at = @At(
+				value = "INVOKE",
+				target = "Lnet/minecraft/server/network/ServerPlayerEntity;getWorld()Lnet/minecraft/world/World;"
+		)
+	)
+	public final World sendToPlayerIfNearby(ServerPlayerEntity player, Operation<World> original) {
+		if ((Object) this instanceof CutsceneWorld cw && cw.getCutscene().hasPlayer(player)) {
+			return this;
+		}
+		return original.call(player);
 	}
 
 }
