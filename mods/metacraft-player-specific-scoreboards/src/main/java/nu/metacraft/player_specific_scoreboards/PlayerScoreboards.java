5ff291078e5ba25ef4518e5f2798cff7b9501500
Acuadragon100 <30689683+Acuadragon100@users.noreply.github.com>
2026-03-16 17:00:37 +0100

    Add function to easily set the scoreboard from code.

--- mods/metacraft-player-specific-scoreboards/src/main/java/nu/metacraft/player_specific_scoreboards/PlayerScoreboards.java
+++ mods/metacraft-player-specific-scoreboards/src/main/java/nu/metacraft/player_specific_scoreboards/PlayerScoreboards.java
@@ -2,7 +2,9 @@ package nu.metacraft.player_specific_scoreboards;
 
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.resources.Identifier;
+import net.minecraft.server.level.ServerPlayer;
 import nu.metacraft.lib.METAcraftLib;
+import nu.metacraft.player_specific_scoreboards.util.PlayerScoreboard;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -19,5 +21,13 @@ public class PlayerScoreboards implements ModInitializer {
 
 	public static Identifier getID(String id) {
 		return Identifier.fromNamespaceAndPath(NAMESPACE, id);
+	}
+
+	public static void setPlayerScoreboard(ServerPlayer player, PlayerScoreboard scoreboard) {
+		((PlayerScoreboardExtension) player).metacraft$setScoreboard(scoreboard);
+	}
+
+	public static void clearPlayerScoreboard(ServerPlayer player) {
+		setPlayerScoreboard(player, null);
 	}
 }
