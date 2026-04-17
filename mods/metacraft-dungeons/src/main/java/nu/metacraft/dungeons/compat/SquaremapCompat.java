ab9f5a96d21157764c6f53f3d836622a2cb85c3a
Acuadragon100 <30689683+Acuadragon100@users.noreply.github.com>
2026-04-17 12:30:25 +0200

    Re-enable Squaremap compatibility.

--- mods/metacraft-dungeons/src/main/java/nu/metacraft/dungeons/compat/SquaremapCompat.java
+++ mods/metacraft-dungeons/src/main/java/nu/metacraft/dungeons/compat/SquaremapCompat.java
@@ -2,22 +2,21 @@ package nu.metacraft.dungeons.compat;
 
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.Level;
-//import xyz.jpenilla.squaremap.api.SquaremapProvider;
-//import xyz.jpenilla.squaremap.api.WorldIdentifier;
-//import xyz.jpenilla.squaremap.common.data.MapWorldInternal;
+import xyz.jpenilla.squaremap.api.SquaremapProvider;
+import xyz.jpenilla.squaremap.api.WorldIdentifier;
+import xyz.jpenilla.squaremap.common.data.MapWorldInternal;
 
 public class SquaremapCompat {
 
 	public static void disableRenderer(ResourceKey<Level> dim) {
-		//FIXME Squaremap
-		/*SquaremapProvider.get().getWorldIfEnabled(WorldIdentifier.parse(dim.identifier().toString())).ifPresent(world -> {
+		SquaremapProvider.get().getWorldIfEnabled(WorldIdentifier.parse(dim.identifier().toString())).ifPresent(world -> {
 			if (world instanceof MapWorldInternal internal) {
 				internal.renderManager().pauseRenders(true);
 				if (internal.renderManager().isRendering()) {
 					internal.renderManager().cancelRender();
 				}
 			}
-		});*/
+		});
 	}
 
 }
--- mods/metacraft-dungeons/src/main/java/nu/metacraft/dungeons/mixin/ConfigManagerMixin.java
+++ mods/metacraft-dungeons/src/main/java/nu/metacraft/dungeons/mixin/ConfigManagerMixin.java
@@ -7,11 +7,10 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import nu.metacraft.dungeons.dungeons.DungeonData;
-//import xyz.jpenilla.squaremap.common.config.ConfigManager;
-//import xyz.jpenilla.squaremap.common.config.WorldConfig;
+import xyz.jpenilla.squaremap.common.config.ConfigManager;
+import xyz.jpenilla.squaremap.common.config.WorldConfig;
 
-//FIXME Squaremap
-/*@Pseudo
+@Pseudo
 @Mixin(ConfigManager.class)
 public class ConfigManagerMixin {
 
@@ -23,4 +22,4 @@ public class ConfigManagerMixin {
 		return config;
 	}
 
-}*/
+}
