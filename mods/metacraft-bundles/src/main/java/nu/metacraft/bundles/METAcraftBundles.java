85b2a7431327be6ca61ba472dfa51cd591a85183
Acuadragon100 <30689683+Acuadragon100@users.noreply.github.com>
2026-09-09 16:09:41 +0200

    Use autohost for bundle rendering, ender relay, wrench and better pets GUI.

--- mods/metacraft-bundles/src/main/java/nu/metacraft/bundles/METAcraftBundles.java
+++ mods/metacraft-bundles/src/main/java/nu/metacraft/bundles/METAcraftBundles.java
@@ -1,5 +1,6 @@
 package nu.metacraft.bundles;
 
+import eu.pb4.polymer.resourcepack.api.PolymerResourcePackUtils;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.resources.Identifier;
 import nu.metacraft.bundles.util.BundleHelper;
@@ -8,13 +9,19 @@ import org.apache.logging.log4j.Logger;
 
 public class METAcraftBundles implements ModInitializer {
 
+	public static final String MODID = "metacraft-bundles";
 	public static final String NAMESPACE = "metacraft";
-	public static final Logger LOGGER = LogManager.getLogger("metacraft-bundles");
+	public static final Logger LOGGER = LogManager.getLogger(MODID);
 
 	@Override
 	public void onInitialize() {
 		BundleComponents.init();
 		BundleHelper.init();
+
+		if (BundleConfig.getInstance().bundleRendering()) {
+			PolymerResourcePackUtils.addModAssets(MODID);
+			PolymerResourcePackUtils.markAsRequired();
+		}
 	}
 
 	public static Identifier getID(String id) {
